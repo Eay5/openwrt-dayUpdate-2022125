8ad4e1b2c7677e5693614b7ac157721c7abbad8b
eay <1015714719@qq.com>
2022-12-05 11:13:59 +0800

    V1

---
+++ 5.15-part1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+###
+ # @Author: eay
+ # @Date: 2022-01-13 17:09:14
+ # @LastEditors: eay
+ # @Autor: Seven
+ # @LastEditTime: 2022-03-05 10:03:42
+ # @Description: 
+### 
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/small-package.git' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-jerrykuku
