b09b767be4654e987b9658c2bdd057d3a565bf95
eay <1015714710@qq.com>
2023-01-06 20:08:27 +0800

    换源 smartdns打开

--- 5.15-part1.sh
+++ 5.15-part1.sh
@@ -22,10 +22,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git helloword https://github.com/fw876/helloworld' feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/small-package.git' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git helloword https://github.com/fw876/helloworld' feeds.conf.default
+# sed -i '$a src-git helloword https://github.com/fw876/helloworld' feeds.conf.default
+# 
+sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-jerrykuku
 #ddns-go
